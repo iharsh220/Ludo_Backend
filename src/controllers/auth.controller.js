62bd91776fa317bf4d88fd0aa6e7a92caf30049f
iharsh220 <hgohil220@gmail.com>
2026-08-04 14:39:00 +0530

    password changed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const Organogram = require('../models/organogram');
 const jwt = require('jsonwebtoken');
 
-const STATIC_PASSWORD = process.env.STATIC_PASSWORD || 'corium' || 'Grogaintab';
+const STATIC_PASSWORD = process.env.STATIC_PASSWORD || 'Grogaintab';
 const JWT_SECRET = process.env.JWT_SECRET || 'corium_jwt_secret';
 const JWT_EXPIRE = process.env.JWT_EXPIRE || '7d';
 
